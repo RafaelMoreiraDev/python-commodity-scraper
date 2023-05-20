0480df6cf08d77b293009ccb51dc102e6873b76b
RAFAEL ANTONIO BRITO MOREIRA <106538987+RafaelMoreiraDev@users.noreply.github.com>
2023-05-19 23:26:29 -0300

    subindo codigo funcional

--- projetoSelenium.py
+++ projetoSelenium.py
@@ -1,22 +1,16 @@
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.service import Service
-
-# servico = Service(executable_path=ChromeDriverManager().install())
-# navegador = webdriver.Chrome(service=servico)
-import time
 import unicodedata
 from selenium import webdriver
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+
 #entrar no site
 servico = Service(executable_path=ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
+
 # #2- importar base de dados
 tabela = pd.read_excel("commodities.xlsx")
 print(tabela)
-
 
 #Tratando erros de padrão br para padrão americano  
 # pra usar na hora de digitar o site
@@ -41,44 +35,7 @@ for i in tabela.index:
     cotacao = cotacao.replace('.','').replace(',','.')
     cotacao = float(cotacao)
     tabela.loc[i,'Preço Atual'] = cotacao
-    # print(tabela)
     tabela["Comprar"] = tabela["Preço Atual"] < tabela["Preço Ideal"]
-
 print(tabela)
 tabela.to_excel("comodities_atualizdo.xlsx", index=False)
 navegador.quit()
-
-
-
-
-
-
-
-
-
-# # Tratar a cotação para não da erro
-# cotacao = cotacao.replace('.','').replace(',','.')
-# cotacao = float(cotacao)
- 
-
-# # na coluna preçoatual preencher com a cotaão
-
-# tabela.loc[0,'Preço Atual'] = cotacao
-# print(tabela)
-
-# # #5- atualizar o preço na base de dados
-# # #6- decidir quaiis produtos vai ser comprado
-# # #7- exportar base de dados
-
-
-# # Seu código aqui
-
-# # time.sleep(60.55) # Espera 5 segundos antes de fechar o navegador
-# # navegador.quit() # Fecha o navegador
-
-
-# # Faça suas ações no navegador aqui
-
-# #time.sleep(5)  # Aguarde 5 segundos antes de fechar o navegador
-
-# #navegador.quit()  # Feche o navegador
